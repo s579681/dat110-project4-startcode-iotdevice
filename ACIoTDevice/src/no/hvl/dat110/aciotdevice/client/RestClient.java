11a28123b31a0e4cf564ea51388a64da80769541
Stian <shauge2206@gmail.com>
2020-05-05 17:20:22 +0200

    task 2

--- ACIoTDevice/src/no/hvl/dat110/aciotdevice/client/RestClient.java
+++ ACIoTDevice/src/no/hvl/dat110/aciotdevice/client/RestClient.java
@@ -1,5 +1,9 @@
 package no.hvl.dat110.aciotdevice.client;
+import okhttp3.*;
 
+import java.io.IOException;
+
+import com.google.gson.Gson;
 public class RestClient {
 
 	public RestClient() {
@@ -7,21 +11,43 @@ public class RestClient {
 	}
 
 	private static String logpath = "/accessdevice/log";
-
+	public static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
 	public void doPostAccessEntry(String message) {
-
-		// TODO: implement a HTTP POST on the service to post the message
+		OkHttpClient client = new OkHttpClient();
+		Gson gson = new Gson();
+		AccessMessage msg = new AccessMessage(message);
+		RequestBody body = RequestBody.create(JSON, gson.toJson(msg));
+		Request req = new Request.Builder().url("http://localhost:8080" + logpath).post(body).build();
+		System.out.println(req);
 		
+		try (Response response = client.newCall(req).execute()) {
+			System.out.println(response.body().string());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	private static String codepath = "/accessdevice/code";
 	
 	public AccessCode doGetAccessCode() {
-
 		AccessCode code = null;
 		
 		// TODO: implement a HTTP GET on the service to get current access code
-		
+		OkHttpClient client = new OkHttpClient();
+		Gson gson = new Gson();
+
+		Request req = new Request.Builder().url("http://localhost:8080" + codepath).get().build();
+
+		System.out.println(req);
+
+		try (Response response = client.newCall(req).execute()) {
+			String body = response.body().string();
+			System.out.println(body);
+			code = gson.fromJson(body, AccessCode.class);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
 		return code;
 	}
 }
